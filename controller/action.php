aed4e2dd84e913f6eeba45e8021ba97e943e8ad3
Akriti Bhadoriya <akriti.bhadoriya96@gmail.com>
2017-05-26 16:55:58 +0530

    Update action.php

--- controller/action.php
+++ controller/action.php
@@ -1,124 +1,129 @@
 <?php
+//cart.php
 session_start();
-/**
- * Created by PhpStorm.
- * User: Vishal Raman
- * Date: 14-Dec-16
- * Time: 21:15 PM
- */
 include_once "../helper/connect.php";
-if(isset($_POST["product_id"]))
-{
-    $order_table = '';
-    $message = '';
-    if($_POST["action"] == "add")
-    {
-        if(isset($_SESSION["shopping_cart"]))
-        {
-            $is_available = 0;
-            foreach($_SESSION["shopping_cart"] as $keys => $values)
-            {
-                if($_SESSION["shopping_cart"][$keys]['product_id'] == $_POST["product_id"])
-                {
-                    $is_available++;
-                    $_SESSION["shopping_cart"][$keys]['product_quantity'] = $_SESSION["shopping_cart"][$keys]['product_quantity'] + $_POST["product_quantity"];
-                }
-            }
-            if($is_available < 1)
-            {
-                $item_array = array(
-                    'product_id'			=>	$_POST["product_id"],
-                    'product_name'			=>	$_POST["product_name"],
-                    'product_price'			=>	$_POST["product_price"],
-                    'product_quantity'		=>	$_POST["product_quantity"]
-                );
-                $_SESSION["shopping_cart"][] = $item_array;
-            }
-        }
-        else
-        {
-            $item_array = array(
-                'product_id'			=>	$_POST["product_id"],
-                'product_name'			=>	$_POST["product_name"],
-                'product_price'			=>	$_POST["product_price"],
-                'product_quantity'		=>	$_POST["product_quantity"]
-            );
-            $_SESSION["shopping_cart"][] = $item_array;
-        }
-    }
-
-    if($_POST["action"] == "remove")
-    {
-        foreach($_SESSION["shopping_cart"] as $keys => $values)
-        {
-            if($values["product_id"] == $_POST["product_id"])
-            {
-                unset($_SESSION["shopping_cart"][$keys]);
-                $message = '<label class="text-success">Product Removed</label>';
-            }
-        }
-    }
-
-    if($_POST["action"] == "quantity_change")
-    {
-        foreach($_SESSION["shopping_cart"] as $keys => $values)
-        {
-            if($_SESSION["shopping_cart"][$keys]['product_id'] == $_POST["product_id"])
-            {
-                $_SESSION["shopping_cart"][$keys]['product_quantity'] = $_POST["quantity"];
-            }
-        }
-    }
-
-    $order_table .= '
-		'.$message.'
-		<table class="table table-bordered">
-			<tr>
-				<th width="40%">Product Name</th>
-				<th width="10%">Quantity</th>
-				<th width="20%">Price</th>
-				<th width="15%">Total</th>
-				<th width="5%">Action</th>
-			</tr>
-		';
-    if(!empty($_SESSION["shopping_cart"]))
-    {
-        $total = 0;
-        foreach($_SESSION["shopping_cart"] as $keys => $values)
-        {
-            $order_table .= '
-				<tr>
-					<td>'.$values["product_name"].'</td>
-					<td><input type="text" name="quantity[]" id="quantity'.$values["product_id"].'" value="'.$values["product_quantity"].'" class="form-control quantity" data-product_id="'.$values["product_id"].'" /></td>
-					<td align="right">Rs. '.$values["product_price"].'</td>
-					<td align="right">Rs. '.number_format($values["product_quantity"] * $values["product_price"], 2).'</td>
-					<td><button name="delete" class="btn btn-danger btn-xs delete" id="'.$values["product_id"].'">Remove</button></td>
-				</tr>
-			';
-            $total = $total + ($values["product_quantity"] * $values["product_price"]);
-        }
-        $order_table .= '
-			<tr>
-				<td colspan="3" align="right">Total</td>
-				<td align="right">Rs. '.number_format($total, 2).'</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td colspan="5" align="center">
-					<form method="post" action="../controller/cart.php">
-						<input type="submit" name="place_order" class="btn btn-warning" value="Place Order" />
-					</form>
-				</td>
-			</tr>
-		';
-    }
-    $order_table .= '</table>';
-
-    $output = array(
-        'order_table'	=>	$order_table,
-        'cart_item'		=>	count($_SESSION["shopping_cart"])
-    );
-    echo json_encode($output);
-}
-
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Order here!</title>
+		<script src="../view/js/jquery.min.js"></script>
+		<link rel="stylesheet" href="../view/css/bootstrap.min.css" />
+		<script src="../view/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<br />
+		<div class="container" style="width:800px;">
+			<?php
+			if(isset($_POST["place_order"]))
+			{
+
+                $Customer_name=$_POST['name'];
+                $email=$_POST['email'];
+                $contact_no=$_POST['contact_no'];
+                $address=$_POST['address'];
+                $landmark=$_POST['landmark'];
+
+			    $insert_customer_details = "
+                INSERT INTO customer_details(Customer_name,email,contact_no,address,landmark) 
+                values ('$Customer_name', '$email', '$contact_no', '$address', '$landmark')";
+
+				$insert_order = "
+				INSERT INTO tbl_order(customer_id, creation_date,  order_status)
+				VALUES('1', '".date('Y-m-d')."', 'pending')      
+				";
+
+				$order_id = "";
+				if(mysqli_query($connect, $insert_order))
+				{
+					$order_id = mysqli_insert_id($connect);
+				}
+				$_SESSION["order_id"] = $order_id;
+				$order_details = "";
+				foreach($_SESSION["shopping_cart"] as $keys => $values)
+				{
+					$order_details .= "
+					INSERT INTO tbl_order_details(order_id, product_name, product_price, product_quantity)
+					VALUES('".$order_id."', '".$values["product_name"]."', '".$values["product_price"]."', '".$values["product_quantity"]."');
+					";
+				}
+
+                if(mysqli_multi_query($connect, $order_details))
+				{
+					unset($_SESSION["shopping_cart"]);
+					echo '<script>alert("You have successfully place an order...Thank you")</script>';
+					echo '<script>window.location.href="cart.php"</script>';
+				}
+			}
+
+			if(isset($_SESSION["order_id"]))
+			{
+				$customer_details = '';
+				$order_details = '';
+				$total = 0;
+				$query = '
+				SELECT * FROM tbl_order
+				INNER JOIN tbl_order_details
+				ON tbl_order_details.order_id = tbl_order.order_id
+				INNER JOIN tbl_customer
+				ON tbl_customer.CustomerID = tbl_order.customer_id
+				WHERE tbl_order.order_id = "'.$_SESSION["order_id"].'"
+				';
+				$result = mysqli_query($connect, $query);
+				while($row = mysqli_fetch_array($result))
+				{
+					$customer_details = '
+					<label>'.$row["CustomerName"].'</label>
+					<p>'.$row["Address"].'</p>
+					<p>'.$row["City"].', '.$row["PostalCode"].'</p>
+					<p>'.$row["Country"].'</p>
+					';
+					$order_details .= "
+						<tr>
+							<td>".$row["product_name"]."</td>
+							<td>".$row["product_quantity"]."</td>
+							<td>".$row["product_price"]."</td>
+							<td>".number_format($row["product_quantity"] * $row["product_price"], 2)."</td>
+						</tr>
+					";
+					$total = $total + ($row["product_quantity"] * $row["product_price"]);
+				}
+				echo '
+				<h3 align="center">Order Summary for Order No.'.$_SESSION["order_id"].'</h3>
+				<div class="table-responsive">
+					<table class="table">
+						<tr>
+							<td><label>Customer Details</label></td>
+						</tr>
+						<tr>
+							<td>'.$customer_details.'</td>
+						</tr>
+						<tr>
+							<td><label>Order Details</label></td>
+						</tr>
+						<tr>
+							<td>
+								<table class="table table-bordered">
+									<tr>
+										<th width="50%">Product Name</th>
+										<th width="15%">Quantity</th>
+										<th width="15%">Price</th>
+										<th width="20%">Total</th>
+									</tr>
+									'.$order_details.'
+									<tr>
+										<td colspan="3" align="right"><label>Total</label></td>
+										<td>'.number_format($total, 2).'</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
+				</div>
+				';
+			}
+			
+			?>
+		</div>
+	</body>
+</html>
