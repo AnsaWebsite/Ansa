595f76dcfb7ebfddfca274ab6b848187218ca5bd
Akriti Bhadoriya <akriti.bhadoriya96@gmail.com>
2017-05-26 17:02:07 +0530

    Update ansa.sql

--- ansa.sql
+++ ansa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2017 at 04:54 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Apr 11, 2017 at 08:31 AM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,11 +14,40 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ansa`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_details`
+--
+
+CREATE TABLE IF NOT EXISTS `customer_details` (
+  `Customer_id` int(11) NOT NULL,
+  `Customer_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact_no` bigint(10) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `landmark` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer_details`
+--
+
+INSERT INTO `customer_details` (`Customer_id`, `Customer_name`, `email`, `contact_no`, `address`, `landmark`) VALUES
+(11395, 'Deepak Kumar Goyal', 'goyaldeepu468@gmail.com', 7575053163, 'tyi', 'vbn'),
+(18047, '', '', 0, '', ''),
+(45930, 'goli', '201452058@iiitvadodara.ac.in', 7567775852, 'vb', 'hj'),
+(57062, '', '', 0, '', ''),
+(57941, 'dee', '2014@gmail.com', 7575053163, 'ghj', 'jkjk'),
+(61278, '', '', 0, '', ''),
+(71394, 'Deepak Kumar Goyal', 'goyaldeepu468@gmail.com', 7575053163, 'ad', 'df'),
+(91425, 'hj', '9@gmail.com', 7575053163, 'yu', 'bn');
 
 -- --------------------------------------------------------
 
@@ -30,8 +59,7 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `id` int(11) NOT NULL,
   `dish_name` varchar(50) NOT NULL,
   `image` varchar(100) NOT NULL,
-  `price` float NOT NULL,
-  PRIMARY KEY (`id`)
+  `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -39,10 +67,10 @@ CREATE TABLE IF NOT EXISTS `menu` (
 --
 
 INSERT INTO `menu` (`id`, `dish_name`, `image`, `price`) VALUES
-(2, 'rkd', 'http://localhost/Ansa_website/view/images/13859.jpg', 78),
-(3, 'egg bhurj', 'http://localhost/Ansa_website/view/images/5308.jpg', 120),
-(4, 'egg curry', 'http://localhost/Ansa_website/view/images/19525.jpg', 130),
-(5, 'egg roll', 'http://localhost/Ansa_website/view/images/31749.jpg', 100);
+(2, 'TEA', 'http://localhost/Ansa_website/view/images/tea.jpg', 15),
+(3, 'COFFEE', 'http://localhost/Ansa_website/view/images/coffee.jpg', 30),
+(4, 'DHOKALA  ', 'http://localhost/Ansa_website/view/images/dhokala.jpg', 40),
+(5, 'SANDWICH', 'http://localhost/Ansa_website/view/images/Sandwich.jpg', 50);
 
 -- --------------------------------------------------------
 
@@ -51,14 +79,13 @@ INSERT INTO `menu` (`id`, `dish_name`, `image`, `price`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_customer` (
-  `CustomerID` int(11) NOT NULL AUTO_INCREMENT,
+  `CustomerID` int(11) NOT NULL,
   `CustomerName` varchar(250) NOT NULL,
   `Address` text NOT NULL,
   `City` varchar(250) NOT NULL,
   `PostalCode` varchar(30) NOT NULL,
-  `Country` varchar(100) NOT NULL,
-  PRIMARY KEY (`CustomerID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `Country` varchar(100) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_customer`
@@ -78,25 +105,26 @@ INSERT INTO `tbl_customer` (`CustomerID`, `CustomerName`, `Address`, `City`, `Po
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_order` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `creation_date` date NOT NULL,
-  `order_status` varchar(50) NOT NULL,
-  PRIMARY KEY (`order_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `order_status` varchar(50) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_order`
 --
 
 INSERT INTO `tbl_order` (`order_id`, `customer_id`, `creation_date`, `order_status`) VALUES
-(1, 1, '2016-12-17', 'pending'),
-(2, 1, '2016-12-17', 'pending'),
-(3, 1, '2016-12-17', 'pending'),
-(4, 1, '2017-01-04', 'pending'),
-(5, 1, '2017-01-06', 'pending'),
-(6, 1, '2017-03-29', 'pending'),
-(7, 1, '2017-04-01', 'pending');
+(22, 1, '2017-04-09', 'pending'),
+(21, 1, '2017-04-09', 'pending'),
+(20, 1, '2017-04-09', 'pending'),
+(19, 1, '2017-04-09', 'pending'),
+(18, 1, '2017-04-08', 'pending'),
+(17, 1, '2017-04-08', 'pending'),
+(16, 1, '2017-04-08', 'pending'),
+(15, 1, '2017-04-08', 'pending'),
+(14, 1, '2017-04-08', 'pending');
 
 -- --------------------------------------------------------
 
@@ -105,31 +133,81 @@ INSERT INTO `tbl_order` (`order_id`, `customer_id`, `creation_date`, `order_stat
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_order_details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_price` double(10,2) NOT NULL,
-  `product_quantity` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `product_quantity` int(11) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_order_details`
 --
 
 INSERT INTO `tbl_order_details` (`id`, `order_id`, `product_name`, `product_price`, `product_quantity`) VALUES
-(1, 1, 'egg', 90.00, 1),
-(2, 1, 'egg roll', 100.00, 1),
-(3, 1, 'egg bhurji', 120.00, 10),
-(4, 2, 'egg', 90.00, 12),
-(5, 3, 'egg', 90.00, 1),
-(6, 4, 'egg', 90.00, 1),
-(7, 4, 'egg roll', 100.00, 1),
-(8, 5, 'rft', 78.00, 6),
-(9, 6, 'rkd', 78.00, 9),
-(10, 6, 'egg curry', 130.00, 10),
-(11, 7, 'rkd', 78.00, 1);
+(29, 22, 'rkd', 78.00, 1),
+(28, 21, 'rkd', 78.00, 1),
+(27, 20, 'egg curry', 130.00, 1),
+(26, 20, 'rkd', 78.00, 1),
+(25, 19, 'egg bhurj', 120.00, 1),
+(24, 18, 'egg bhurj', 120.00, 1),
+(23, 16, 'rkd', 78.00, 1),
+(22, 15, 'egg curry', 130.00, 5),
+(21, 14, 'egg roll', 100.00, 4);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer_details`
+--
+ALTER TABLE `customer_details`
+  ADD PRIMARY KEY (`Customer_id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `tbl_order_details`
+--
+ALTER TABLE `tbl_order_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `tbl_order_details`
+--
+ALTER TABLE `tbl_order_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
